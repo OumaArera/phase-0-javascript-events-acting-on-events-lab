646b152e56214f04b4171ababc3fbb0cd84af561
OumaArera <johnouma999@gmail.com>
2023-12-20 13:47:43 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+const moveDodgerLeft = () => {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+
+  document.addEventListener("keydown", e => {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+
+    }
+  });
+
+
+  const moveDodgerRight = () => {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(rightNumbers, 10);
+
+    if (left < 180){
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", event => {
+    if (event.key === "ArrowRight"){
+        moveDodgerRight();
+    }
+})
+
